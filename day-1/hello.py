d3ce104e2fce1d61f48f034bcf4e376792c899b1
Asabeneh <asabeneh@gmail.com>
2021-08-04 20:34:39 +0300

    additonal python scripts

--- day-1/hello.py
+++ day-1/hello.py
@@ -1,2 +1,17 @@
-print("Dereje")
-print("python is awesome")
+first_name = 'Dereje'
+last_name = 'Dinku'
+country = 'Ethiopia'
+course = 'Python Bootcamp'
+
+full_name = first_name + ' ' + last_name
+print(full_name, country, course)
+print("Python is awesome!")
+
+# this is a python comment and let's do arithmetics
+
+print( 1 + 99)
+print(100 - 99)
+print( 2 * 3)
+print(3 / 2)
+print(3 ** 3)
+print( 2 ** 10)
